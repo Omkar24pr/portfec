bdd401e859c3ea318cce1ff578905939fd3aca6f
Omkar24pr <153834536+Omkar24pr@users.noreply.github.com>
2024-03-28 23:49:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const list=document.querySelector(".links");
+const lines=document.querySelector(".fa-bars");
+const cross=document.querySelector(".fa-xmark");
+
+lines.addEventListener("click", ()=>{
+      lines.style.display="none";
+      cross.style.display="block";
+      list.style.display="flex";
+});
+
+cross.addEventListener("click", ()=>{
+    lines.style.display="block";
+    cross.style.display="none";
+    list.style.display="none";
+});
+
+const width=screen.width;
